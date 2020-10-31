87057f20a6a2c1bea5a8ae7834538643452dffb1
包凯瑞 <673575209@qq.com>
2020-11-01 00:43:24 +0800

    simplelayout第四次提交

--- simplelayout.py
+++ simplelayout.py
@@ -9,7 +9,7 @@ def main():
     parser.add_argument('--board_grid', metavar='', type=int, help='矩形区域的边长像素数')
     parser.add_argument('--unit_grid', metavar='', type=int, help='矩形组件分辨率')
     parser.add_argument('--unit_n', metavar='', type=int, help='组件数')
-    parser.add_argument('--positions', metavar='', type=int, help='每个组件的位置编号')
+    parser.add_argument('--positions', metavar='', nargs='+', type=int, help='每个组件的位置编号')
     parser.add_argument('--outdir', metavar='', type=str, help='输出结果的目录')
     parser.add_argument('--file_name', metavar='', type=str, help='输出文件名')
     args = parser.parse_args()
@@ -19,10 +19,10 @@ def main():
     if (args.board_grid % args.unit_grid != 0):
         sys.exit(1)
 
-    if (args.positions < 1 or args.positions > b_n):
+    if (len(args.positions) < 1 or len(args.positions) > b_n):
         sys.exit(1)
 
-    if (args.positions != args.unit_n):
+    if (len(args.positions) != args.unit_n):
         sys.exit(1)
 
     if not isExists:
